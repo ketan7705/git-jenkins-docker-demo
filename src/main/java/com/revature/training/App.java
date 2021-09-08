03e551e80472efd7991965a47a5ac0d93cff5f1a
ketan shinde <ssanket2001@gmail.com>
2021-09-08 11:59:43 +0530

    first commit

---
+++ src/main/java/com/revature/training/App.java
@@ -0,0 +1,14 @@
+package com.revature.training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("First Jenkins Project");
+    }
+}
